b749715fcc9cd4bb7fb9d1133a3f0ab44718d5d8
junhong1923 <m81923@gmail.com>
2021-08-19 01:12:02 +0800

    Try different return type of ActionResult in MoviesController

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //return View(movie); // put the movie model in the view so it can render it
+            //return Content("Return plain string content in the response.");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            
+            // use anonymous object to pass arg to the target action, arg will appear as query string
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+    }
+}
