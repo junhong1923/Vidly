4822c84d9b27403e7e327de39c69b00ed5b32b4a
junhong1923 <m81923@gmail.com>
2021-08-21 23:32:57 +0800

    Used View Models and Razor View

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,14 +14,28 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-
             //return View(movie); // put the movie model in the view so it can render it
             //return Content("Return plain string content in the response.");
             //return HttpNotFound();
             //return new EmptyResult();
-            
+
             // use anonymous object to pass arg to the target action, arg will appear as query string
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+
+            // ViewModel
+            var customers = new List<Customer> // object initialization syntax
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel // create ViewModel object and init properties
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
         //  GET: movies/edit?id=1 or movies/edit/1
@@ -41,6 +56,12 @@ namespace Vidly.Controllers
                 sortBy = "Name";
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")] // Attribute-based Routing
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
