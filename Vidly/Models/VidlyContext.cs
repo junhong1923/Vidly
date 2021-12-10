ec6d20973a60bd17055eb9f88957586f9af20351
junhong1923 <m81923@gmail.com>
2021-12-10 09:00:54 +0800

    Install EF and enable-migration with InitialModel

---
+++ Vidly/Models/VidlyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace Vidly.Models
+{
+    // Since No context type was found in the assembly 'Vidly', we add this Context class
+    public class VidlyContext : DbContext
+    {
+        public VidlyContext()
+        {
+
+        }
+
+        // Entities that I want to create databases from as properties of the class
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
